69a718e9cee89e9eafc2dd77d934cc41a73f8fde
Mvrouvne <marouanechaibat@gmail.com>
2024-05-05 23:40:29 +0100

    finished setting wordpress

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -1,2 +1,29 @@
 sleep 10;
 
+wp config create --allow-root \
+            --dbname=$SQL_DATABASE \
+            --dbuser=$SQL_DATABASE \
+            --dbpass=$SQL_PASSWORD \
+            --dbhost=mariadb:3306 \
+            --path='/var/www/wordpress'
+
+wp install --allow-root \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USR \
+        --admin_password=$WP_ADMIN_PASS \
+        --admin_email=$WP_ADMIN_EMAIL
+
+wp user create --allow-root \
+            $WP_USR $WP_USR_EMAIL \
+            --role=author \
+            --user_pass=$WP_USR_PASS
+
+wp theme install --allow-root OceanWP \
+            --force --activate
+
+wp plugin install --allow-root jetpack \
+            --force --activate
+
+mkdir -p /run/php
+
+/usr/sbin/php-fpm7.3 -F
