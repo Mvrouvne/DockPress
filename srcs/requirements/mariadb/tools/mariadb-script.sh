72aedee6bf2ba4876d9765603481070e6e699443
Mvrouvne <marouanechaibat@gmail.com>
2024-05-26 12:46:33 +0100

    minor update in mariadb setup script

--- srcs/requirements/mariadb/tools/mariadb-script.sh
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -2,7 +2,7 @@
 
 echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" > /etc/mysql/init.sql
 echo "CREATE USER IF NOT EXISTS $SQL_USER@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> /etc/mysql/init.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO $SQL_USER@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
+echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER@'%';" >> /etc/mysql/init.sql
 echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
 
 mysqld
